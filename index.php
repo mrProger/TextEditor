1873485fa63d4546e6e6a4653b8989fdf31cf0b5
Ilya Khrebtov <zkhrebtovilya@gmail.com>
2022-09-25 21:09:39 +0600

    Добавлены маршруты с методом POST для загрузки и чтения файла | Добавлена страница для загрузки файла | Добавлено автоматическое добавление текста при загрузке маршрута editor

--- index.php
+++ index.php
@@ -8,26 +8,32 @@ include __DIR__ . '/app/PHPView/View.php';
 use PHPRouter\Router;
 use PHPTemplater\Template;
 use PHPView\View;
+use PHPExceptionHandler\ExceptionHandler;
 
 $router = new Router();
 
-$router->get("test", function() {
-    echo "<h3>test</h3>";
-    $content = file_get_contents(__DIR__ . "/file.txt");
-    if ($content != null) {
-        echo $content;
-    } else {
-        echo "Не удалось прочитать файл";
-    }
+$router->post("load-file", function() {
+    move_uploaded_file($_FILES["file"]["tmp_name"], "file.txt");
+    echo "<script>document.location = 'editor'</script>";
 });
 
-$router->post("test", function() {
-    var_dump($_FILES);
-    move_uploaded_file($_FILES["file"]["tmp_name"], "file.txt");
+$router->post("read-file", function() {
+    $content = file_get_contents(__DIR__ . "/file.txt");
+
+    if ($content == null) {
+        ExceptionHandler::generateError("Не удалось прочитать загруженный файл");
+    }
+
+    echo $content;
+});
+
+$router->get("editor", function() {
+    $template = new Template(__DIR__ . '/pages/editor.html');
+    echo View::createFromTemplate($template);
 });
 
 $router->get("/", function() {
-    $template = new Template(__DIR__ . '/pages/editor.html');
+    $template = new Template(__DIR__ . '/pages/index.html');
     echo View::createFromTemplate($template);
 });
 
